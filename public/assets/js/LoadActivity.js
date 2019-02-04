0905c9e347d0847bb609f8665096ef6cf9c54ccd
JamieMeininger <jamie_meininger@hotmail.co.uk>
2019-02-04 13:47:43 +0000

    updates applied

--- public/assets/js/LoadActivity.js
+++ public/assets/js/LoadActivity.js
@@ -1,16 +1,40 @@
+var currentActivity;
 $( document ).ready(function() {
+    $('#continue').click(nextActivity);
+    $('.ActivityLink').click(bottomMenuLink);
+    LoadActivity(1,"Load");
 
-   LoadActivity(1);
-    
 });
 
-function LoadActivity(ActivityNum){
-     $.get("https://foundationlearning.eu-gb.mybluemix.net/course-EC101/Lesson-1/"+ActivityNum, function(data, status){
-         $( "#LessonContainer" ).append( data.activityHTML );
-         $('#Title').html(data.longName)
+function LoadActivity(ActivityNum,tag){
+    currentActivity = ActivityNum;
+    if(tag != "Load"){
+    //$( "#LessonContainer" ).animate({}, 2000);
+    }
+
+    $.get("https://foundationlearning.eu-gb.mybluemix.net/course-EC101/Lesson-1/"+ActivityNum, function(data, status){
+         $( "#LessonContainer" ).animate({opacity: 0}, 500,function(){
+             $( "#LessonContainer" ).html( data.activityHTML );
+             $('#Title').html(data.longName)
+             $( "#LessonContainer" ).animate({opacity: 1},500)
+         })
      });
      var BottomList = $('#BottomMenu div')
      $('#BottomMenu div').removeClass("OnPage");
-      $('#BottomMenu div:nth-child('+ActivityNum+')').removeClass("InactiveLink");
+     $('#BottomMenu div:nth-child('+ActivityNum+') a').removeClass("InactiveLink");
+     $('#BottomMenu div:nth-child('+ActivityNum+') ').removeClass("InactiveLink");
      $('#BottomMenu div:nth-child('+ActivityNum+')').addClass("OnPage");
+}
+
+function nextActivity(){
+    LoadActivity(parseInt(currentActivity) + 1)
+}
+
+function bottomMenuLink(){
+    if(!($(this).hasClass('InactiveLink'))){
+        LoadActivity($(this).attr('href'))
+    }
+
+
+    return false;
 }
