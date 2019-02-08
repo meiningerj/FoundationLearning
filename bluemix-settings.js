ac0148223951169e353f5c37ed31cb2f824b6076
meiningerj <jamie_meininger@hotmail.co.uk>
2019-02-08 09:58:39 +0000

    Update bluemix-settings.js

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -32,7 +32,7 @@ var settings = module.exports = {
     uiPort: process.env.PORT || 1880,
     mqttReconnectTime: 15000,
     debugMaxLength: 1000,
-    httpNodeCors: {origin: "*",methods: "GET,PUT,POST,DELETE"},
+    httpNodeCors: {origin: "*",methods: "GET,PUT,POST,DELETE", credentials: true},
     userDir: userDir,
 
     flowFile: "flows.json",
